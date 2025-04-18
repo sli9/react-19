27d7a083bee5aec260aade4ad6949cd3abf3dcf2
sli9 <sil9gosy@mail.ru>
2025-04-18 22:45:27 +0300

    Configure project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <main>
+      <div className={'pattern'} />
+      <div className={'wrapper'}>
+        <header>
+          <h1>
+            <span className={'text-gradient'}>Movie</span> App
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+}
+
+export default App;
